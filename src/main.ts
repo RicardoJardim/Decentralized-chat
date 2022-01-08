13f413f146a5b36ab51781ddd2770c277a588393
Ricardo <ricardo.l.jardim@hotmail.com>
2022-01-08 18:53:17 +0000

    vuex and gun js

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
 import { createApp } from "vue";
+import store from "./store";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
+import GUN from "gun";
+import "gun/sea";
+import "gun/axe";
 
-createApp(App).use(router).mount("#app");
+// Database
+export const db = GUN();
+
+// Gun User
+export const user = db.user().recall({ sessionStorage: true });
+
+/* user.get("alias").on((v) => {
+    UserName = v;
+}); */
+
+createApp(App).use(store).use(router).mount("#app");
---
+++ src/views/Login.vue
@@ -0,0 +1,171 @@
+<template>
+  <div id="login">
+    <form @submit.prevent="">
+      <div class="row">
+        <div class="col-25">
+          <label for="username">Username</label>
+        </div>
+        <div class="col-75">
+          <input
+            name="username"
+            type="text"
+            v-model="username"
+            minlength="3"
+            maxlength="16"
+          />
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-25">
+          <label for="password">Password</label>
+        </div>
+        <div class="col-75">
+          <input name="password" v-model="password" type="password" />
+        </div>
+      </div>
+      <button class="button" v-on:click="login">Login</button>
+      <br />
+      <button class="button" v-on:click="signup">Sign Up</button>
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+import { user } from "@/main";
+import { defineComponent } from "vue";
+import store from "../store";
+
+export default defineComponent({
+  name: "Home",
+  components: {},
+  data(): { username: string; password: string } {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    increment() {
+      store.commit("increment");
+      console.log(store.state.count);
+    },
+    async login() {
+      user.auth(this.username, this.password, async () => {
+        const alias = await user.get("alias"); // username string
+
+        console.log(`signed in as ${alias}`);
+      });
+    },
+    async signup() {
+      this.increment();
+      console.log(this.username, this.password);
+      user.create(
+        this.username,
+        this.password,
+        (data: { ok: 0; pub: string } | { err: string }) => {
+          if ("err" in data) {
+            alert(data.err);
+          } else {
+            this.login();
+          }
+        }
+      );
+    },
+  },
+});
+</script>
+
+<style>
+#login {
+  display: block;
+  background: #fff;
+  margin: 10rem 5rem;
+  padding: 1rem;
+  padding-top: 0;
+  position: relative;
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 2.5rem 5rem 0 rgba(0, 0, 0, 0.1);
+}
+.button {
+  background-color: #4caf50; /* Green */
+  border: none;
+  border-radius: 20px;
+  color: white;
+  padding: 15px 32px;
+  width: 10rem;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+  -webkit-transition-duration: 0.4s; /* Safari */
+  transition-duration: 0.4s;
+}
+
+.button:hover {
+  box-shadow: 0 12px 16px 0 rgba(0, 0, 0, 0.24), 0 17px 50px 0 rgba(0, 0, 0, 0.19);
+}
+
+@media screen and (min-width: 550px) {
+  #login {
+    padding: 4rem;
+  }
+}
+#login > * {
+  max-width: 50rem;
+  margin-left: auto;
+  margin-right: auto;
+}
+#login > form {
+  max-width: 100%;
+}
+#login h1 {
+  display: block;
+  min-width: 100%;
+  width: 100%;
+  text-align: center;
+  margin: 0;
+  margin-bottom: 1rem;
+}
+
+input[type="text"],
+input[type="password"] {
+  width: 75%;
+  padding: 12px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  resize: vertical;
+}
+
+label {
+  padding: 12px 12px 12px 0;
+  display: inline-block;
+}
+.col-25 {
+  float: left;
+  width: 25%;
+  margin-top: 6px;
+}
+
+.col-75 {
+  float: left;
+  width: 75%;
+  margin-top: 6px;
+}
+
+/* Clear floats after the columns */
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+/* Responsive layout - when the screen is less than 600px wide, make the two columns stack on top of each other instead of next to each other */
+@media screen and (max-width: 600px) {
+  .col-25,
+  .col-75 {
+    width: 100%;
+    margin-top: 0;
+  }
+}
+</style>
